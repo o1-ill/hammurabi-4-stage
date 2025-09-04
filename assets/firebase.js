ed5aad2b0cbbc9f4eb474a40ea083304575142a1
Ahmed AlMusawi <ahm7.8.2003@gmail.com>
2025-09-05 00:40:23 +0300

    Update firebase.js

--- assets/firebase.js
+++ assets/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-database.js";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCDZZqUzXRAwh3RKaGp1unluBWYMWF2Q7s",
   authDomain: "hammurabi-ccf6d.firebaseapp.com",
@@ -12,7 +13,6 @@ const firebaseConfig = {
   appId: "1:257971367886:web:761013b57defa59a3b459f",
   measurementId: "G-KS3SWR8LYC"
 };
-
 // تهيئة Firebase
 const app = initializeApp(firebaseConfig);
 
